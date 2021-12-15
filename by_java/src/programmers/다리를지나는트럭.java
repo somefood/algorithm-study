dba656458a5bb0d9e9dcac1348b79437e5de00a9
Seokju Hong <iamseokjuhong@gmail.com>
2021-12-15 21:19:17 +0900

    [프래그래머스] 다리를지나는트럭

--- by_java/src/programmers/다리를지나는트럭.java
+++ by_java/src/programmers/다리를지나는트럭.java
@@ -1,34 +1,39 @@
 package programmers;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class 다리를지나는트럭 {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
+        Queue<Integer> queue = new LinkedList<>();
         int answer = 0;
-        Queue<Integer> completed = new LinkedList<>();
-        Queue<Integer> ing = new LinkedList<>();
-        Queue<Integer> start = new LinkedList<>();
-        for (int truck_weight : truck_weights) {
-            start.add(truck_weight);
+        int sum = 0;
+
+        for (int truck : truck_weights) {
+            while (true) {
+                if(queue.isEmpty()) {
+                    queue.add(truck);
+                    sum += truck;
+                    answer++;
+                    break;
+                } else if(queue.size() == bridge_length) {
+                    sum -= queue.poll();;
+                } else {
+                    if (sum + truck <= weight) {
+                        queue.add(truck);
+                        sum += truck;
+                        answer++;
+                        break;
+                    } else {
+                        queue.add(0);
+                        answer++;
+                    }
+                }
+            }
         }
 
-        while(!start.isEmpty()) {
-            int truck = start.peek();
-            int bridgeTotal = 0;
-            for (Integer integer : ing) {
-                bridgeTotal += integer;
-            }
-            if((bridgeTotal + truck) < weight) {
-                ing.add(truck);
-                start.poll();
-            }
-            answer++;
-            if((answer % bridge_length) == 0) {
-                ing.poll();
-            }
-        }
-        return answer;
+        return answer + bridge_length;
     }
 
     public static void main(String[] args) {
