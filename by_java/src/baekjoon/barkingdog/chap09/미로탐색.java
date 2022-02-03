4f98b3e3a6c81ca84b91cdcb6aac81968488c2ef
Seokju Hong <iamseokjuhong@gmail.com>
2022-02-03 21:14:02 +0900

    [백준] 미로탐색

--- by_java/src/baekjoon/barkingdog/chap09/미로탐색.java
+++ by_java/src/baekjoon/barkingdog/chap09/미로탐색.java
@@ -1,5 +1,6 @@
 package baekjoon.barkingdog.chap09;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -7,40 +8,38 @@ import java.util.Scanner;
 public class 미로탐색 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int x = scanner.nextInt();
-        int y = scanner.nextInt();
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+        int[][] board = new int[N][M];
+        int[][] visited = new int[N][M];
 
-        int[][] map = new int[x][y];
-
-        for(int i=0; i<x; i++){
-            String[] split = scanner.next().split("");
-            for(int j=0; j<split.length; j++) {
-                map[i][j] = Integer.parseInt(split[j]);
+        for(int i=0; i<N; i++) {
+            String s = scanner.next();
+            for(int j=0; j<s.length(); j++) {
+                board[i][j] = s.charAt(j) - '0';
             }
         }
 
-        int[][] visited = new int[x][y];
-        visited[0][0] = 1;
-        Queue<int[]> Q = new LinkedList<>();
-        Q.add(new int[]{0, 0});
-
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
+        Queue<int[]> Q = new LinkedList<>();
+        visited[0][0] = 1;
+        Q.add(new int[]{0, 0});
 
         while(!Q.isEmpty()) {
             int[] cur = Q.poll();
+            for(int i=0; i<4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
 
-            for(int dir=0; dir<4; dir++) {
-                int nx = cur[0] + dx[dir];
-                int ny = cur[1] + dy[dir];
-
-                if(nx < 0 || nx >= x || ny < 0 || ny >= y) continue;
-                if(map[nx][ny] != 1 || visited[nx][ny] >= 1 ) continue;
+                if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+                if(board[nx][ny] == 0 || visited[nx][ny] != 0) continue;
 
                 visited[nx][ny] = visited[cur[0]][cur[1]] + 1;
                 Q.add(new int[]{nx, ny});
             }
         }
-        System.out.println(visited[x-1][y-1]);
+
+        System.out.println(visited[N-1][M-1]);
     }
 }
