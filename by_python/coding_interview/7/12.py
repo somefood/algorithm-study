eedc8ea42c2c01f02b4494503b8ae86410816c2a
Seokju Hong <iamseokjuhong@gmail.com>
2022-03-07 23:26:09 +0900

    [코딩인터뷰] 7장 배열

--- by_python/coding_interview/7/12.py
+++ by_python/coding_interview/7/12.py
@@ -1,3 +1,4 @@
+import sys
 from typing import List
 
 
@@ -11,8 +12,22 @@ class Solution:
 
         return max_price
 
+    def book_solution(self, prices: List[int]) -> int:
+        profit = 0
+        min_price = sys.maxsize
+
+        for price in prices:
+            min_price = min(min_price, price)
+            profit = max(profit, price - min_price)
+
+        return profit
+
 
 s = Solution()
-print(s.maxProfit([7, 1, 5, 3, 6, 4]))  # 5
-print(s.maxProfit([7, 6, 4, 3, 1]))  # 5
-print(s.maxProfit([2, 4, 1]))  # 5
+# print(s.maxProfit([7, 1, 5, 3, 6, 4]))  # 5
+# print(s.maxProfit([7, 6, 4, 3, 1]))  # 5
+# print(s.maxProfit([2, 4, 1]))  # 5
+
+print(s.book_solution([7, 1, 5, 3, 6, 4]))  # 5
+# print(s.book_solution([7, 6, 4, 3, 1]))  # 5
+# print(s.book_solution([2, 4, 1]))  # 5
