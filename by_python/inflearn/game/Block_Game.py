0b985461b940bc1fcdfbab89162bcdf2a60f45f0
Seokju Hong <iamseokjuhong@gmail.com>
2022-05-25 16:47:41 +0900

    [인프런, Game] STEP 2 : 터틀그래픽을 이용한 블록 떨어뜨리기

--- by_python/inflearn/game/Block_Game.py
+++ by_python/inflearn/game/Block_Game.py
@@ -1,8 +1,17 @@
+import time
 import turtle as t
 import random as r
 
 
+class Brick:
+    def __init__(self):
+        self.y = 0
+        self.x = 6
+        self.color = r.randint(1, 6)
+
+
 def draw_grid(block, grid):
+    block.clear()
     top = 250
     left = -150
     colors = ["black", "red", "blue", "orange", "yellow", "green", "purple", "white"]
@@ -17,6 +26,7 @@ def draw_grid(block, grid):
 
 if __name__ == "__main__":
     sc = t.Screen()
+    sc.tracer(False)
     sc.bgcolor("black")
     sc.setup(width=600, height=700)
 
@@ -25,15 +35,31 @@ if __name__ == "__main__":
         grid[i].insert(0, 7)
         grid[i].append(7)
     grid.append([7] * 14)
+    for y in range(23, 20, -1):
+        for x in range(1, 13):
+            grid[y][x] = r.randint(1, 6)
 
     block = t.Turtle()
     block.penup()
     block.speed(0)
     block.shape("square")
     block.color("red")
+    block.setundobuffer(None)
 
+    brick = Brick()
+    grid[brick.y][brick.x] = brick.color
     draw_grid(block, grid)
 
-    for x in grid:
-        print(x)
+    while True:
+        sc.update()
+        if grid[brick.y + 1][brick.x] == 0:
+            grid[brick.y][brick.x] = 0
+            brick.y += 1
+            grid[brick.y][brick.x] = brick.color
+        for x in grid:
+            print(x)
+        print()
+        draw_grid(block, grid)
+        time.sleep(0.5)
+
     sc.mainloop() # 계속 띄우기
