f01bd9a42a89757328d340ef55c592fc55535196
Seokju Hong <iamseokjuhong@gmail.com>
2022-07-23 23:11:28 +0900

    [인프런] 문제 다시 풀이

---
+++ by_python/inflearn/restudy/2_코드구현력/1.py
@@ -0,0 +1,15 @@
+n, k = map(int, input().split())
+result = []
+for i in range(1, n + 1):
+    if n % i == 0:
+        result.append(i)
+
+if k <= len(result):
+    print(result[k-1])
+else:
+    print(-1)
+
+
+"""
+6 3
+"""
