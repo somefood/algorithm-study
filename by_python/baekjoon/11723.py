5851dd33a5bfc3e3e5e48f7b3ed9710108b8c00e
Seokju Hong <iamseokjuhong@gmail.com>
2022-07-29 18:34:22 +0900

    [백준] 기초 실버 풀이

---
+++ by_python/baekjoon/11723.py
@@ -0,0 +1,61 @@
+import sys
+
+m = int(sys.stdin.readline())
+S = set()
+
+for _ in range(m):
+    temp = sys.stdin.readline().strip().split()
+
+    if len(temp) == 1:
+        if temp[0] == "all":
+            S = set([i for i in range(1, 21)])
+        else:
+            S = set()
+
+    else:
+        func, x = temp[0], temp[1]
+        x = int(x)
+
+        if func == "add":
+            S.add(x)
+        elif func == "remove":
+            S.discard(x)
+        elif func == "check":
+            print(1 if x in S else 0)
+        elif func == "toggle":
+            if x in S:
+                S.discard(x)
+            else:
+                S.add(x)
+
+
+
+"""
+26
+add 1
+add 2
+check 1
+check 2
+check 3
+remove 2
+check 1
+check 2
+toggle 3
+check 1
+check 2
+check 3
+check 4
+all
+check 10
+check 20
+toggle 10
+remove 20
+check 10
+check 20
+empty
+check 1
+toggle 1
+check 1
+toggle 1
+check 1
+"""
