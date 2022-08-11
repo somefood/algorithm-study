08e9412b40b8a222b9da5ecf21be8db06d52cd9c
Seokju Hong <iamseokjuhong@gmail.com>
2022-08-11 11:51:42 +0900

    [이코테] 다익스트라, 플로이드 문제 풀이

---
+++ by_python/이것이코딩테스트다/9_최단경로/미래도시.py
@@ -0,0 +1,48 @@
+import sys
+
+input = sys.stdin.readline
+INF = int(1e9)
+
+n, m = map(int, input().split())
+
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for a in range(n + 1):
+    for b in range(n + 1):
+        if a == b:
+            graph[a][b] = 0
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+
+x, k = map(int, input().split())
+
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+
+if graph[1][k] >= INF or graph[k][x] >= INF:
+    print(-1)
+else:
+    print(graph[1][k] + graph[k][x])
+
+"""
+5 7 
+1 2
+1 3
+1 4
+2 4
+3 4
+3 5
+4 5
+4 5
+
+4 2
+1 3
+2 4
+3 4
+"""
